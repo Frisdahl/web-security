3ce201b4ec94200b406e9fceee6837ae726738c1
Frisdahl <afrisser3@gmail.com>
2025-05-27 23:55:50 +0200

    updated database.php

--- app/database.php
+++ app/database.php
@@ -1,7 +1,21 @@
 <?php
-$pdo = new PDO(
-    'mysql:host=mysql-db;port=3306;dbname=web-security-application;charset=utf8mb4',
-    'admin',
-    'password'
-);
+
+try {
+    $options = [
+        PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt', // Render har typisk dette certifikat
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ];
+
+    $pdo = new PDO(
+        'mysql:host=' . getenv('DB_HOST') .
+        ';port=' . getenv('DB_PORT') .
+        ';dbname=' . getenv('DB_NAME') .
+        ';charset=utf8mb4',
+        getenv('DB_USER'),
+        getenv('DB_PASS'),
+        $options
+    );
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
 ?>
